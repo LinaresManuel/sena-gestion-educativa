8966127a20d8d3c5a27e69482b07e48e43b4ba1f
Manuel Linares <95196081+LinaresManuel@users.noreply.github.com>
2026-07-01 11:39:05 -0500

    fix: wrapper global de fetch con credentials include

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+const originalFetch = window.fetch.bind(window);
+window.fetch = ((input: RequestInfo | URL, init: RequestInit = {}) => {
+  return originalFetch(input, { credentials: 'include', ...init });
+}) as typeof window.fetch;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
